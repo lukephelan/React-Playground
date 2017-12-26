6443b1fa486bf9f99292d11aae59d814254499cf
Luke Phelan <luke.phelan@hotmail.com>
2017-12-26 13:33:58 +1100

    Add a HelloWorld component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +23,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <HelloWorld />
       </div>
     );
   }
